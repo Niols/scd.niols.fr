a363146b755e902ef012abd960b4e69a60aa0e1a
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2021-12-05 20:24:58 +0100

    better dance page

--- src/build.sh
+++ src/build.sh
@@ -23,16 +23,22 @@ ls -1 "$DB"/dance | while read dance; do
 
     echo "  - compile TeX to PDF"
     ( cd "$BUILD"/dance
-      xelatex "$dance".tex
-    ) > "$BUILD"/dance/"$dance"
+      xelatex "$dance".tex \
+        > "$dance".xelatex-log )
+
+    echo "  - generate JSON file"
+    { cat "$DB"/dance/"$dance"/meta.json \
+      | jq "setpath([\"slug\"]; \"$dance\")"
+    } > "$BUILD"/dance/"$dance".json
 
     echo "  - generate Mustache file"
-    { cat "$SRC"/html/dance.html
+    { cat "$SRC"/html/header.html
+      cat "$SRC"/html/dance.html
+      cat "$SRC"/html/footer.html
     } > "$BUILD"/dance/"$dance".mustache
 
     echo "  - compile Mustache to HTML"
-    mustache \
-      "$DB"/dance/"$dance"/meta.json \
-      "$BUILD"/dance/"$dance".mustache \
-      > "$BUILD"/dance/"$dance".html
-done
+    ( cd "$BUILD"/dance
+      mustache "$dance".json "$dance".mustache \
+        > "$dance".html )
+    done
