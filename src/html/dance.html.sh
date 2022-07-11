25fa9ed953a4959483e2a9c9f3ced60b28315e60
Niols <niols@niols.fr>
2022-07-11 20:03:00 +0200

    Cleanup: `printf --` -> `printf`

--- src/html/dance.html.sh
+++ src/html/dance.html.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-printf -- '<article>'
-printf -- '  <h1>%s</h1>' "$(html name)"
-printf -- '  <h2>%s</h2>' "$(html kind)"
-printf -- '  <section>'
-printf -- '    <p>Devised by %s.</p>' "$(html author)"
-printf -- '    <p>See the <a href="%s.pdf">description of the dance</a>.</p>' "$(url slug)"
+printf '<article>'
+printf   '<h1>%s</h1>' "$(html name)"
+printf   '<h2>%s</h2>' "$(html kind)"
+
+printf   '<section>'
+printf     '<p>Devised by %s.</p>' "$(html author)"
+printf     '<p>See the <a href="%s.pdf">description of the dance</a>.</p>' "$(url slug)"
 
 if exists scddb-id; then
-    printf -- '<p>See this dance'
-    printf -- '  <a href="https://my.strathspey.org/dd/dance/%s/">' "$(url scddb-id)"
-    printf -- '    in the Scottish Country Dance Database</a>.</p>'
+    printf '<p>See this dance'
+    printf  ' <a href="https://my.strathspey.org/dd/dance/%s/">' "$(url scddb-id)"
+    printf     'in the Scottish Country Dance Database</a>.</p>'
 fi
 
-printf -- '  </section>'
-printf -- '</article>'
+printf   '</section>'
+printf '</article>'
--- src/html/footer.html.sh
+++ src/html/footer.html.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 
-printf -- '  </body>'
-printf -- '</html>'
+printf   '</body>'
+printf '</html>'
