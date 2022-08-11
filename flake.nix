8a79055d47c9dec04dd1d10b337187c3a5b77ea3
Niols <niols@niols.fr>
2022-08-11 13:44:36 +0200

    Add a basic flake that makes `nix develop` work
    somehow it still fails with `nix shell` and `nix build` because of fonts
    stuff but that will come eventually

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Flake for building and developing scd.niols.fr";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux; in
+    {
+      packages.x86_64-linux.default =
+        with import nixpkgs { system = "x86_64-linux"; };
+        stdenv.mkDerivation {
+          name = "scd.niols.fr";
+          src = self;
+
+          buildInputs = [
+            pkgs.firefox      ## for tests
+            pkgs.imagemagick  ## for tests
+            pkgs.inkscape
+            pkgs.jq
+            pkgs.lilypond
+            pkgs.sassc
+            pkgs.texlive.combined.scheme-full
+            pkgs.yq-go
+          ];
+
+          FONTCONFIG_FILE = makeFontsConf { fontDirectories = [
+            self.packages.x86_64-linux.trebuchetms ]; };
+
+          buildPhase = "make website";
+          installPhase = "mkdir -p $out/var/www && cp -R _build/website $out/var/www";
+        };
+
+      packages.x86_64-linux.trebuchetms =
+        with import nixpkgs { system = "x86_64-linux"; };
+        stdenv.mkDerivation {
+          name = "trebuchetms";
+          src = self;
+
+          installPhase = "install -m444 -Dt $out/share/fonts assets/fonts/*.ttf";
+        };
+    };
+}
