a62e1005a6370851fbf60767db1f0107d130f358
Niols <niols@niols.fr>
2022-10-05 14:56:55 +0200

    Factorise `FONTCONFIG_FILE`

--- flake.nix
+++ flake.nix
@@ -11,6 +11,12 @@
             src = self;
           } // args);
 
+        mkDerivationWithFonts = args:
+          mkDerivation ({
+            FONTCONFIG_FILE = pkgs.makeFontsConf { fontDirectories = [
+              self.packages.trebuchetms ]; };
+          } // args);
+
         websiteBuildInputs = [
           pkgs.inkscape
           pkgs.jq
@@ -37,19 +43,19 @@
 
       packages.default = self.packages.website;
 
-      devShell = mkDerivation {
+      devShell = mkDerivationWithFonts {
         name = "devshell";
         buildInputs = websiteBuildInputs ++ websiteTestInputs;
       };
 
-      packages.website = mkDerivation {
+      packages.website = mkDerivationWithFonts {
         name = "website";
         buildInputs = websiteBuildInputs;
         buildPhase = "make website";
         installPhase = "mkdir -p $out/var && cp -R _build/website $out/var/www";
       };
 
-      packages.test-website = mkDerivation {
+      packages.test-website = mkDerivationWithFonts {
         name = "test-website";
         buildInputs = websiteBuildInputs;
         buildPhase = "make test-website";
