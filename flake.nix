106a956f026a6355032703afc7cf2ac4160a5002
Niols <niols@niols.fr>
2022-12-30 20:25:52 +0100

    Flake utiles

--- flake.nix
+++ flake.nix
@@ -1,73 +1,68 @@
 {
-  description = "Flake for building and developing scd.niols.fr";
+  inputs.flake-utils.url = github:numtide/flake-utils;
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
 
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      let pkgs = import nixpkgs { inherit system; };
 
-        mkDerivation = args:
-          pkgs.stdenv.mkDerivation ({
-            src = self;
-            FONTCONFIG_FILE = pkgs.makeFontsConf { fontDirectories = [
-              pkgs.google-fonts ]; };
-          } // args);
+          mkDerivation = args:
+            pkgs.stdenv.mkDerivation ({
+              src = self;
+              FONTCONFIG_FILE = pkgs.makeFontsConf { fontDirectories = [
+                pkgs.google-fonts ]; };
+            } // args);
 
-        websiteBuildInputs = [
-          pkgs.inkscape
-          pkgs.j2cli
-          pkgs.jq
-          pkgs.lilypond
-          pkgs.sassc
-          pkgs.texlive.combined.scheme-full
-          pkgs.xvfb-run
-          pkgs.yq-go
-        ];
+          websiteBuildInputs = [
+            pkgs.inkscape
+            pkgs.j2cli
+            pkgs.jq
+            pkgs.lilypond
+            pkgs.sassc
+            pkgs.texlive.combined.scheme-full
+            pkgs.xvfb-run
+            pkgs.yq-go
+          ];
 
-        websiteTestInputs = [
-          pkgs.firefox
-          pkgs.imagemagick
-          #pkgs.python310 ## is implied by:
-          pkgs.python310Packages.selenium
-        ];
-    in
+          websiteTestInputs = [
+            pkgs.firefox
+            pkgs.imagemagick
+            #pkgs.python310 ## is implied by:
+            pkgs.python310Packages.selenium
+          ];
+      in
 
-    {
-      packages.x86_64-linux.default = self.packages.default;
-      packages.x86_64-linux.website = self.packages.website;
-      packages.x86_64-linux.test-website = self.packages.test-website;
-      packages.x86_64-linux.tests = self.packages.tests;
-      devShells.x86_64-linux.default = self.devShell;
+        {
+          packages.default = self.packages.${system}.website;
 
-      packages.default = self.packages.website;
+          devShells.default = mkDerivation {
+            name = "devshell";
+            buildInputs = websiteBuildInputs ++ websiteTestInputs;
+          };
 
-      devShell = mkDerivation {
-        name = "devshell";
-        buildInputs = websiteBuildInputs ++ websiteTestInputs;
-      };
+          packages.website = mkDerivation {
+            name = "website";
+            buildInputs = websiteBuildInputs;
+            buildPhase = "make website";
+            installPhase = "mkdir $out && cp -R _build/website/* $out/";
+          };
 
-      packages.website = mkDerivation {
-        name = "website";
-        buildInputs = websiteBuildInputs;
-        buildPhase = "make website";
-        installPhase = "mkdir $out && cp -R _build/website/* $out/";
-      };
+          packages.test-website = mkDerivation {
+            name = "test-website";
+            buildInputs = websiteBuildInputs;
+            buildPhase = "make test-website";
+            installPhase = "mkdir $out && cp -R _build/website/* $out/";
+          };
 
-      packages.test-website = mkDerivation {
-        name = "test-website";
-        buildInputs = websiteBuildInputs;
-        buildPhase = "make test-website";
-        installPhase = "mkdir $out && cp -R _build/website/* $out/";
-      };
-
-      packages.tests = mkDerivation {
-        name = "tests";
-        buildInputs = websiteBuildInputs ++ websiteTestInputs;
-        buildPhase = ''
+          packages.tests = mkDerivation {
+            name = "tests";
+            buildInputs = websiteBuildInputs ++ websiteTestInputs;
+            buildPhase = ''
           export HOME=$(mktemp -d)
           make tests website-output=${self.packages.test-website}/
         '';
-        installPhase = "mkdir $out && cp -R _build/tests/* $out/";
-      };
-    };
+            installPhase = "mkdir $out && cp -R _build/tests/* $out/";
+          };
+        }
+    );
 }
