9a4b354c8ea56366f62fd3764a92ebdc28006f0b
Nicolas Jeannerod <niols@niols.fr>
2023-02-11 23:59:20 +0100

    Rework and improve Nix environment (#150)
    * Enter flake-parts
    
    * Run `nixfmt`
    
    * Move `mkDerivation` to own lib
    
    * Enable pre-commit hooks
    
    * Rework CI to use Cachix
    
    * Enable cache in flake
    
    * Try TeXlive medium
    
    * Try TeXlive small
    
    * Try TeXlive basic
    
    * Custom TeXlive
    
    * Use action to create PR comment
    
    * +enumitem
    
    * Enable action for testing purposes
    
    * github.event.issue.number
    
    * github.event.number
    
    * +wrapfig
    
    * Comment token
    
    * Missing slash
    
    * Re-disable action
    
    * Run `prettier` on CSS files
    
    * Run `prettier` on YAML files
    
    * Run flake checks in CI as well
    
    * Move `nixConfig` to proper place in flake
    
    * Base on `scheme-small`
    
    * Move packages to own files
    
    * Breaaathe

---
+++ .nix/package-website.nix
@@ -0,0 +1,39 @@
+{ self, ... }: {
+  perSystem = { self', pkgs, ... }:
+    let
+      mkDerivation = self.lib.mkDerivationFor pkgs;
+
+      customTexlive = pkgs.texlive.combine {
+        inherit (pkgs.texlive)
+          scheme-small enumitem ifoddpage tikzpagenodes wrapfig xifthen;
+      };
+
+      websiteBuildInputs = [
+        pkgs.inkscape
+        pkgs.j2cli
+        pkgs.jq
+        pkgs.lilypond
+        pkgs.sassc
+        customTexlive
+        pkgs.xvfb-run
+        pkgs.yq-go
+      ];
+
+    in {
+      packages.default = self'.packages.website;
+
+      packages.website = mkDerivation {
+        name = "website";
+        buildInputs = websiteBuildInputs;
+        buildPhase = "make website";
+        installPhase = "mkdir -p $out/var && cp -R _build/website $out/var/www";
+      };
+
+      packages.test-website = mkDerivation {
+        name = "test-website";
+        buildInputs = websiteBuildInputs;
+        buildPhase = "make test-website";
+        installPhase = "mkdir -p $out/var && cp -R _build/website $out/var/www";
+      };
+    };
+}
--- flake.nix
+++ flake.nix
@@ -1,65 +1,24 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
 
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-
-        mkDerivation = args:
-          pkgs.stdenv.mkDerivation ({
-            src = self;
-            FONTCONFIG_FILE = pkgs.makeFontsConf { fontDirectories = [
-              pkgs.google-fonts ]; };
-            ## Do not look this derivation up in substitutes, because it is
-            ## never going to be there.
-            allowSubstitutes = false;
-          } // args);
-
-        websiteBuildInputs = [
-          pkgs.inkscape
-          pkgs.j2cli
-          pkgs.jq
-          pkgs.lilypond
-          pkgs.sassc
-          pkgs.texlive.combined.scheme-full
-          pkgs.xvfb-run
-          pkgs.yq-go
-        ];
-
-        websiteTestInputs = [
-          pkgs.firefox
-          pkgs.imagemagick
-          #pkgs.python310 ## is implied by:
-          pkgs.python310Packages.selenium
-        ];
-    in
-
-    {
-      packages.x86_64-linux.default = self.packages.default;
-      packages.x86_64-linux.website = self.packages.website;
-      packages.x86_64-linux.test-website = self.packages.test-website;
-      devShells.x86_64-linux.default = self.devShell;
-
-      packages.default = self.packages.website;
-
-      devShell = mkDerivation {
-        name = "devshell";
-        buildInputs = websiteBuildInputs ++ websiteTestInputs;
-        buildPhase = "true";
-        installPhase = "mkdir $out";
-      };
-
-      packages.website = mkDerivation {
-        name = "website";
-        buildInputs = websiteBuildInputs;
-        buildPhase = "make website";
-        installPhase = "mkdir -p $out/var && cp -R _build/website $out/var/www";
-      };
-
-      packages.test-website = mkDerivation {
-        name = "test-website";
-        buildInputs = websiteBuildInputs;
-        buildPhase = "make test-website";
-        installPhase = "mkdir -p $out/var && cp -R _build/website $out/var/www";
-      };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.pre-commit-hooks.flakeModule
+        ./.nix/lib.nix
+        ./.nix/pre-commit-settings.nix
+        ./.nix/package-website.nix
+        ./.nix/devShell.nix
+      ];
+      systems = [ "x86_64-linux" ];
     };
+
+  nixConfig = {
+    extra-trusted-substituters = [ "https://scd-niols-fr.cachix.org/" ];
+    extra-trusted-public-keys = [
+      "scd-niols-fr.cachix.org-1:7NP/UmPtYppVv3Qq7C6MNLL6jVL8x8bcPB96NckZpDw="
+    ];
+  };
 }
