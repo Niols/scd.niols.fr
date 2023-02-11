65687be6c33c6cb3a24fa5114abfcbe43c9a0cce
Niols <niols@niols.fr>
2023-02-11 18:51:12 +0100

    Move packages to own files

---
+++ .nix/devShell.nix
@@ -0,0 +1,21 @@
+{ self, ... }: {
+  perSystem = { self', config, pkgs, ... }:
+    let
+      mkDerivation = self.lib.mkDerivationFor pkgs;
+      websiteBuildInputs = self'.packages.website.buildInputs;
+      websiteTestInputs = [
+        pkgs.firefox
+        pkgs.imagemagick
+        #pkgs.python310 ## is implied by:
+        pkgs.python310Packages.selenium
+      ];
+    in {
+      devShells.default = mkDerivation {
+        name = "devshell";
+        buildInputs = websiteBuildInputs ++ websiteTestInputs;
+        buildPhase = "true";
+        installPhase = "mkdir $out";
+        shellHook = config.pre-commit.installationScript;
+      };
+    };
+}
--- flake.nix
+++ flake.nix
@@ -3,70 +3,16 @@
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
 
-  outputs = inputs@{ self, flake-parts, ... }:
+  outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.pre-commit-hooks.flakeModule
         ./.nix/lib.nix
         ./.nix/pre-commit-settings.nix
+        ./.nix/package-website.nix
+        ./.nix/devShell.nix
       ];
-
       systems = [ "x86_64-linux" ];
-
-      perSystem = { self', config, pkgs, ... }:
-        let
-          mkDerivation = self.lib.mkDerivationFor pkgs;
-
-          customTexlive = pkgs.texlive.combine {
-            inherit (pkgs.texlive)
-              scheme-small enumitem ifoddpage tikzpagenodes wrapfig xifthen;
-          };
-
-          websiteBuildInputs = [
-            pkgs.inkscape
-            pkgs.j2cli
-            pkgs.jq
-            pkgs.lilypond
-            pkgs.sassc
-            customTexlive
-            pkgs.xvfb-run
-            pkgs.yq-go
-          ];
-
-          websiteTestInputs = [
-            pkgs.firefox
-            pkgs.imagemagick
-            #pkgs.python310 ## is implied by:
-            pkgs.python310Packages.selenium
-          ];
-
-        in {
-          packages.default = self'.packages.website;
-
-          devShells.default = mkDerivation {
-            name = "devshell";
-            buildInputs = websiteBuildInputs ++ websiteTestInputs;
-            buildPhase = "true";
-            installPhase = "mkdir $out";
-            shellHook = config.pre-commit.installationScript;
-          };
-
-          packages.website = mkDerivation {
-            name = "website";
-            buildInputs = websiteBuildInputs;
-            buildPhase = "make website";
-            installPhase =
-              "mkdir -p $out/var && cp -R _build/website $out/var/www";
-          };
-
-          packages.test-website = mkDerivation {
-            name = "test-website";
-            buildInputs = websiteBuildInputs;
-            buildPhase = "make test-website";
-            installPhase =
-              "mkdir -p $out/var && cp -R _build/website $out/var/www";
-          };
-        };
     };
 
   nixConfig = {
