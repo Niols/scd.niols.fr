a228451b3b73e611429397a37f3b092d31da7a2e
Niols <niols@niols.fr>
2023-02-11 19:45:02 +0100

    Breaaathe

--- .nix/devShell.nix
+++ .nix/devShell.nix
@@ -2,13 +2,16 @@
   perSystem = { self', config, pkgs, ... }:
     let
       mkDerivation = self.lib.mkDerivationFor pkgs;
+
       websiteBuildInputs = self'.packages.website.buildInputs;
+
       websiteTestInputs = [
         pkgs.firefox
         pkgs.imagemagick
         #pkgs.python310 ## is implied by:
         pkgs.python310Packages.selenium
       ];
+
     in {
       devShells.default = mkDerivation {
         name = "devshell";
