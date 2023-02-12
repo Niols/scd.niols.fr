0b52b485dd6ea70881a2a35b8bdb75882c612e75
Niols <niols@niols.fr>
2023-02-12 15:34:32 +0100

    Google Fonts with only Source Sans Pro
    see also https://github.com/NixOS/nixpkgs/pull/215335

--- .nix/lib.nix
+++ .nix/lib.nix
@@ -1,9 +1,12 @@
 { self, ... }: {
   flake.lib.mkDerivationFor = pkgs: args:
-    pkgs.stdenv.mkDerivation ({
+    let
+      my-google-fonts =
+        pkgs.google-fonts.override { fonts = [ "Source Sans Pro" ]; };
+    in pkgs.stdenv.mkDerivation ({
       src = self;
       FONTCONFIG_FILE =
-        pkgs.makeFontsConf { fontDirectories = [ pkgs.google-fonts ]; };
+        pkgs.makeFontsConf { fontDirectories = [ my-google-fonts ]; };
       ## Do not look this derivation up in substitutes, because it is
       ## never going to be there.
       ## REVIEW: Should change if we introduce Cachix.
