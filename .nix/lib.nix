2cee8eceff13f13941eb9165a806289c0c01506c
Niols <niols@niols.fr>
2023-03-09 16:12:46 +0100

    Prepare for other `flake.lib` attributes

--- .nix/lib.nix
+++ .nix/lib.nix
@@ -1,12 +1,14 @@
 { self, ... }: {
-  flake.lib.mkDerivationFor = pkgs: args:
-    pkgs.stdenv.mkDerivation ({
-      src = self;
-      FONTCONFIG_FILE =
-        pkgs.makeFontsConf { fontDirectories = [ pkgs.google-fonts ]; };
-      ## Do not look this derivation up in substitutes, because it is
-      ## never going to be there.
-      ## REVIEW: Should change if we introduce Cachix.
-      allowSubstitutes = false;
-    } // args);
+  flake.lib = {
+    mkDerivationFor = pkgs: args:
+      pkgs.stdenv.mkDerivation ({
+        src = self;
+        FONTCONFIG_FILE =
+          pkgs.makeFontsConf { fontDirectories = [ pkgs.google-fonts ]; };
+        ## Do not look this derivation up in substitutes, because it is
+        ## never going to be there.
+        ## REVIEW: Should change if we introduce Cachix.
+        allowSubstitutes = false;
+      } // args);
+  };
 }
