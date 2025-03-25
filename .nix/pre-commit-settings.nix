9292137c2ff0f548ec5fc6ae9fa278ca9ed7d558
Niols <niols@niols.fr>
2025-03-25 11:52:09 +0100

    Rename `.yml` files to `.yaml`

--- .nix/pre-commit-settings.nix
+++ .nix/pre-commit-settings.nix
@@ -1,9 +1,29 @@
-{ ... }: {
-  perSystem = { ... }: {
+{ ... }:
+
+{
+  perSystem = { pkgs, ... }: {
     pre-commit.settings.hooks = {
       nixfmt-classic.enable = true;
       deadnix.enable = true;
       prettier.enable = true;
+
+      yml-to-yaml = {
+        enable = true;
+        name = ".yml to .yaml";
+        entry = let
+          yml-to-yaml = pkgs.writeShellApplication {
+            name = "yml-to-yaml";
+            text = ''
+              for file; do
+                truncated=''${file%.yml}
+                if [ "$truncated" != "$file" ]; then
+                  mv "$file" "$truncated".yaml
+                fi
+              done
+            '';
+          };
+        in "${yml-to-yaml}/bin/yml-to-yaml";
+      };
     };
   };
 }
